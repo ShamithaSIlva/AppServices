67db803f483a09a095a091fb50c8e8b20edf55b4
ShamithaSilva <shamithadevinda@gmail.com>
2020-10-18 23:50:04 +0100

    resource server change

--- src/main/java/com/example/security/ResourceServerConfiguration.java
+++ src/main/java/com/example/security/ResourceServerConfiguration.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -101,7 +102,7 @@ public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter
 	@Override
 	public void configure( HttpSecurity http ) throws Exception
 	{
-		http.authorizeRequests().antMatchers( "/oauth/token", "/oauth/authorize**" ).permitAll();
+		http.csrf().disable().authorizeRequests().antMatchers( "/oauth/token", "/oauth/authorize**" ).permitAll();
 		http.requestMatchers().antMatchers( "/tasks/**" ).and().authorizeRequests().antMatchers( "/tasks/**" ).access( "hasRole('USER')" );
 	}
 }
